1727f546667952b17d17ef17467ea7a937ded17e
subfare <71392621+subfare@users.noreply.github.com>
2024-11-20 10:01:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,5 @@ document.querySelectorAll('.dropdown-toggle').forEach((button) => {
     button.addEventListener('click', (event) => {
         const dropdown = event.target.closest('.dropdown');
         dropdown.classList.toggle('open');
-
-        // Close other open dropdowns
-        document.querySelectorAll('.dropdown').forEach((otherDropdown) => {
-            if (otherDropdown !== dropdown) {
-                otherDropdown.classList.remove('open');
-            }
-        });
     });
-});
-
-// Close dropdowns when clicking outside
-window.addEventListener('click', (event) => {
-    if (!event.target.closest('.dropdown')) {
-        document.querySelectorAll('.dropdown').forEach((dropdown) => {
-            dropdown.classList.remove('open');
-        });
-    }
 });
